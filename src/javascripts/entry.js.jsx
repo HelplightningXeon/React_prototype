61b038394881e0b240c656e5d213a93a85466b8e
HelplightningXeon <xeon.wang@helplightning.com>
2015-12-28 15:48:01 +0800

    Refactor to use Router

--- src/javascripts/entry.js.jsx
+++ src/javascripts/entry.js.jsx
@@ -5,7 +5,10 @@ require("../css/font-awesome.min.scss");
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
 
-var Calls = require('./components/calls/calls.js.jsx');
+//ReactDOM.render(<Calls src="http://localhost:32769/calls/retrieve_calls" />, document.getElementById('example'));
+var Router = require('react-router').Router;
+var routes = require('./routes.js.jsx');
 
-ReactDOM.render(<Calls src="http://localhost:32769/calls/retrieve_calls" />, document.getElementById('example'));
+ReactDOM.render(<Router routes={routes} />, document.getElementById('example'));
---
+++ src/javascripts/routes.js.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var Route = require('react-router').Route;
+var Calls = require('./components/calls/calls.js.jsx');
+
+module.exports = (
+    <Route path="/" component={Calls} />
+);
+
